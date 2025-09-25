1b5a15be70d178c802187137dbc5324c139efc26
baptiste <baptiste.demenorval@ynov.com>
2025-09-25 16:02:26 +0200

    feat(simplefonction): updated the function to be able to have as many arguments as the user wants

--- simplefonction.go
+++ simplefonction.go
@@ -2,11 +2,15 @@ package main
 
 import "fmt"
 
-func somme(a int, b int) int {
-	return a + b
+func somme(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
 }
 
 func main() {
-	result := somme(3, 5)
+	result := somme(3, 5, 7, 9, 11)
 	fmt.Println("The result is :", result)
 }
