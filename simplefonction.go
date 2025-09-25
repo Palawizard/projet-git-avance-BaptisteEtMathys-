3ec28438e848984940f7ec650043b254908a1646
pmathys <you@example.com>
2025-09-25 15:05:44 +0200

    feat(fonction-b): adding a simple fonction

---
+++ simplefonction.go
@@ -0,0 +1,12 @@
+package projet_git_avance_BaptisteEtMathys_
+
+import "fmt"
+
+func addition(a int, b int) int {
+	return a + b
+}
+
+func main() {
+	resultat := addition(3, 5)
+	fmt.Println("Le résultat est :", resultat)
+}
